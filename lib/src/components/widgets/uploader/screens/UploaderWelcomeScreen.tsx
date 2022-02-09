2543ab95e8b8e979f47208162c8a8b149932915d
Lawrence Wagerfield <lawrence@projectone.io>
2022-02-09 18:25:01 +0000

    Remove progress icon from welcome screen

--- lib/src/components/widgets/uploader/screens/UploaderWelcomeScreen.tsx
+++ lib/src/components/widgets/uploader/screens/UploaderWelcomeScreen.tsx
@@ -1,34 +1,20 @@
 import { UploadButton } from "uploader/components/widgets/uploader/components/UploadButton";
 import { JSX } from "preact";
 import { UploaderParamsRequired } from "uploader/UploaderParams";
-import { ProgressIcon } from "uploader/components/widgets/uploader/components/fileIcons/ProgressIcon";
-import { useEffect, useState } from "preact/compat";
-import unknownSvg from "uploader/components/widgets/uploader/components/fileIcons/svgs/Unknown.svg";
 
 interface Props {
   addFiles: (files: File[]) => void;
   params: UploaderParamsRequired;
 }
 
-export const UploaderWelcomeScreen = ({ addFiles, params: { multi, locale } }: Props): JSX.Element => {
-  const [progress, setProgress] = useState(0);
-
-  useEffect(() => {
-    setInterval(() => {
-      setProgress(p => Math.min(1, p + 0.2));
-    }, 300);
-  }, []);
-
-  return (
-    <>
-      <ProgressIcon progress={progress} onCompleteImageSource={unknownSvg} height={40} />
-      <UploadButton
-        multi={multi}
-        text={multi ? locale.uploadFiles : locale.uploadFile}
-        className="btn--primary btn--upload"
-        onUpload={addFiles}
-      />
-      <p>{locale.orDragDropCopyPaste}</p>
-    </>
-  );
-};
+export const UploaderWelcomeScreen = ({ addFiles, params: { multi, locale } }: Props): JSX.Element => (
+  <>
+    <UploadButton
+      multi={multi}
+      text={multi ? locale.uploadFiles : locale.uploadFile}
+      className="btn--primary btn--upload"
+      onUpload={addFiles}
+    />
+    <p>{locale.orDragDropCopyPaste}</p>
+  </>
+);
