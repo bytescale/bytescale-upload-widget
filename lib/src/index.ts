d6b0bec3e682d362d3a7924af9a24c3e0e98a94f
Lawrence Wagerfield <lawrence@projectone.io>
2022-03-07 22:33:00 +0000

    Add helper error for initializing uploader

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,2 +1,16 @@
+import { Uploader } from "uploader/Uploader";
+
 export { Uploader } from "uploader/Uploader";
 export * from "upload-js";
+
+if (typeof window !== "undefined") {
+  const dummy: Record<keyof Uploader, () => void> = {
+    open(): void {
+      throw new Error(
+        "You forgot to initialize Uploader!\nPlease add:\nconst uploader = new Uploader({ apiKey: 'free'});"
+      );
+    }
+  };
+
+  (window as any).uploader = dummy;
+}
