20802b89687f3e8a9002c18fc3d463135feb72f4
Lawrence Wagerfield <lawrence@projectone.io>
2023-05-08 13:15:25 +0100

    Fix spinner layout breaking on wide screens

--- lib/src/components/widgets/uploader/components/editors/Spinner.tsx
+++ lib/src/components/widgets/uploader/components/editors/Spinner.tsx
@@ -1,8 +1,16 @@
 import { JSX } from "preact";
 import "./Spinner.scss";
+import { getElementDimensionsOnResize } from "uploader/modules/common/UseDimensionsFromElement";
 
-export const Spinner = (): JSX.Element => (
-  <div class="spinner__container">
-    <div className="spinner"></div>
-  </div>
-);
+export const Spinner = (): JSX.Element => {
+  const [dimensions, containerRef] = getElementDimensionsOnResize(true, []);
+  const relativeSize = 0.5;
+  const lowestDim = Math.min(dimensions?.width ?? 0, dimensions?.height ?? 0);
+  const lowestDimCss = `${Math.round(lowestDim * relativeSize)}px`;
+
+  return (
+    <div class="spinner__container" ref={containerRef}>
+      <div className="spinner" style={{ width: lowestDimCss, height: lowestDimCss }}></div>
+    </div>
+  );
+};
