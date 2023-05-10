d92d7041cc5298459648b943af5de0e5335622ab
Lawrence Wagerfield <lawrence@projectone.io>
2023-05-10 13:56:46 +0100

    Add some waits for test

--- lib/tests/e2e/specs/app.spec.ts
+++ lib/tests/e2e/specs/app.spec.ts
@@ -4,8 +4,10 @@ describe("uploader sandbox", () => {
   it("should contain a file upload button", async () => {
     await App.open();
     // @ts-expect-error
-    await browser.pause(1000); // Wait for on-click handler to be registered.
+    await browser.pause(3000); // Wait for on-click handler to be registered.
     await App.modalLauncher.click();
+    // @ts-expect-error
+    await browser.pause(3000); // Wait for modal to appear.
     // @ts-expect-error
     await expect(App.modalButton).toHaveText("Upload an Image");
   });
