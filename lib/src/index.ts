25b4ebeaa46e3bc7c1a95810873281439b12c265
Lawrence Wagerfield <lawrence@projectone.io>
2023-07-31 10:17:13 +0100

    Remove window.uploader check

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,5 +1,3 @@
-import { UploaderInterface } from "uploader/UploaderInterface";
-
 export { Uploader } from "uploader/Uploader";
 export { UploaderInterface } from "uploader/UploaderInterface";
 export { UploadWidgetResult } from "uploader/components/modal/UploadWidgetResult";
@@ -9,15 +7,3 @@ export { UploadWidgetEditor } from "uploader/config/UploadWidgetEditor";
 export { UploadWidgetConfig } from "uploader/config/UploadWidgetConfig";
 export { UploadWidgetMethods } from "uploader/config/UploadWidgetMethods";
 export { UploaderLocaleEnUs } from "uploader/modules/locales/EN_US";
-
-if (typeof window !== "undefined") {
-  const dummy: Partial<Record<keyof UploaderInterface, () => void>> = {
-    open(): void {
-      throw new Error(
-        "You forgot to initialize Uploader!\nPlease initialize 'uploader' with:\nconst uploader = Uploader({ apiKey: 'free' });"
-      );
-    }
-  };
-
-  (window as any).uploader = dummy;
-}
